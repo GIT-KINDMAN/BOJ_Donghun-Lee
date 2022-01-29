84753e3c95e656f55ada8dd835aee5a2166ed2f6
GIT-KINDMAN <95545623+GIT-KINDMAN@users.noreply.github.com>
2022-01-29 21:19:17 +0900

    22.01.29

---
+++ src/BOJ_2422/LDH_2422.java
@@ -0,0 +1,30 @@
+package BOJ_2422;
+
+import java.util.Scanner;
+
+public class LDH_2422 {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        Star(N);
+
+        sc.close();
+    }
+    
+    private static void Star(int N) {
+        System.out.println("문제5)");
+        for (int i = 1; i <= N; i++) {
+            for (int j = N - i; j >= 1; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (i - 1) * 2 + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
