c0fb77cffd8b59cc508e5308c7f3f67dab4a6b02
GIT-KINDMAN <mankind1990s@gmail.com>
2022-02-14 00:16:18 +0900

    22.02.14

--- src/BOJ_2470/LDH_2470.java
+++ src/BOJ_2470/LDH_2470.java
@@ -8,7 +8,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class LDH_2470 {
 
     static int resL;
     static int resR;
