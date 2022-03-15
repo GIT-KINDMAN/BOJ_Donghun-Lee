2a5d48c7868bcb5f02fb30288a4d77c43e5ba3f2
GIT-KINDMAN <mankind1990s@gmail.com>
2022-03-15 17:24:49 +0900

    22.03.15

---
+++ src/BOJ_15651/Main.java
@@ -0,0 +1,43 @@
+package BOJ_15651;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth) {
+        if (depth == M) {
+            for (int i=0; i<M; i++) {
+                sb.append(arr[i]+" ");
+            }
+            sb.append("\n");
+            //System.out.println(sb);
+            //sb.delete(0,sb.length());
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            arr[depth] = i+1;
+            //sb.append(i+1+" ");
+            dfs(depth + 1);
+        }
+    }
+}
